13519cadd148885a0427ac4120f85f8cccc631cc
luquocphap <phap.luquoc@hcmut.edu.vn>
2025-10-08 20:32:14 +0700

    change

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 // use absolute paths so views resolve correctly from src/
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
